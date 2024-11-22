138df7da5c53f317465a1351cd3309d96712f541
Priyanshi662 <priyanshiw567@gmail.com>
2024-11-23 00:11:20 +0530

    intial commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { ModeToggle } from "./modeToggle";
 import { Button } from "./ui/button";
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-
 export default function Header()
 {
     return(
